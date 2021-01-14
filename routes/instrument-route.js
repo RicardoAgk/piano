dcd64e90d0051fbf04cbbee6e5d17096a99dfdd7
Ricardo Andre <ricardoandre.dasein@gmail.com>
2021-01-14 13:53:33 +0000

    simple

--- routes/instrument-route.js
+++ routes/instrument-route.js
@@ -6,12 +6,15 @@ const Profile = require('../models/profile-model')
 const passport = require('passport');
 
 
-  router.put('/profile', (req,res) => {
+  router.put('/profile/:id', (req,res) => {
     const profileId = req.params.id;
-    const profileWithNewDetails = req.body
+    const instrument = req.body.instrument
+    console.log(profileId)
+    console.log(req.body)
   
-    Profile.findByIdAndUpdate(profileId, profileWithNewDetails)
+    Profile.findByIdAndUpdate(profileId, {$push:{instruments: instrument}})
     .then(() => {
+      console.log(response.data)
       res.json({ message: `${req.params.id}'s new instrument was added.` })
     })
   });
